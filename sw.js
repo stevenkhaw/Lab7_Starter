0702c53ad181986cae7fd6ff3c449af1f28be72d
Steven Khaw <swkhaw8226@yahoo.com>
2022-11-12 12:02:01 -0800

    complete

--- sw.js
+++ sw.js
@@ -37,23 +37,22 @@ self.addEventListener('fetch', function (event) {
   //       fetch(event.request)
   // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
   /*******************************/
+
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
-
   event.respondWith(caches.open(CACHE_NAME).then((cache) => {
-    return cache.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request.url).then((fetchedResponse) => {
-        cache.put(event.request, fetchedResponse.clone());
+    // Go to the network first
+    return fetch(event.request).then((fetchedResponse) => {
+      cache.put(event.request, fetchedResponse.clone());
 
-        return fetchedResponse;
-      });
+      return fetchedResponse;
+
+    // B8. TODO - If the request is in the cache, return with the cached version.
+    //            Otherwise fetch the resource, add it to the cache, and return
+    //            network response.
+    }).catch(() => {
+      // If the network is unavailable, get
+      return cache.match(event.request);
     });
   }));
-
-  
-
-
-  // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
 });
